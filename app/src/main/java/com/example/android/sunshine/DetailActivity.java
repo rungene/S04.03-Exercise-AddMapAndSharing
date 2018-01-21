9b3206011d21c96b110a8f6f989a45512e805dd5
rungene <rungenesamuel@gmail.com>
2018-01-21 03:15:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+        return true;
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mForecast+ FORECAST_SHARE_HASHTAG)
+                .getIntent();
+
+        return shareIntent;
+
+
+    }
+    /*    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+          //  shareText();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+    // TODO (3) Create a menu with an item with id of action_share
+    // TODO (4) Display the menu and implement the forecast sharing functionality
+
+/*    private void shareText(String FORECAST_SHARE_HASHTAG){
+
+        String mimeType = "text/plain";
+
+        String title ="Learning how to Share";
+
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(title)
+                .setText(mForecast+FORECAST_SHARE_HASHTAG)
+                .startChooser();
+
+    }*/
+}
